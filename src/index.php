7d4f63984fdf80e7ca24ef8592eee3fa227b204a
Libre Life <me@librelife.org>
2025-11-23 23:04:56 -0500

    Created *very* simple index page

---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php require $_SERVER['DOCUMENT_ROOT'] . '/modules/head.html'?>
+<body>
+<p>
+	<!-- This is where you place your HTML -->
+</p>
+</body>
+<?php require $_SERVER['DOCUMENT_ROOT'] . '/modules/foot.html'?>
