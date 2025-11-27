a8d93ae7021e15e0427ca747a0e88e92657f0b3c
Libre Life <me@librelife.org>
2025-11-26 23:32:32 -0500

    Centering page

--- src/index.php
+++ src/index.php
@@ -3,6 +3,8 @@
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/modules/nav.html'?>
 <p>
 	<!-- This is where you place your HTML -->
+This is a test!!
+
 </p>
 </body>
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/modules/foot.html'?>
