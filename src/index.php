4e6c6fe8e8268431b085303522a9de0795b720d9
Libre Life <me@librelife.org>
2025-11-26 22:56:49 -0500

    Created styles file

--- src/index.php
+++ src/index.php
@@ -1,5 +1,6 @@
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/modules/head.html'?>
 <body>
+<?php require $_SERVER['DOCUMENT_ROOT'] . '/modules/nav.html'?>
 <p>
 	<!-- This is where you place your HTML -->
 </p>
